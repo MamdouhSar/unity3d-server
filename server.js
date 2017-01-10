42eed82028c77959fc1b0214df6c8d15037f6848
MamdouhSar <mamdoh.saraireh@yahoo.com>
2017-01-10 20:57:31 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -2,14 +2,28 @@
  * Created by appleuser3 on 1/10/17.
  */
 var express = require('express');
-var app = express();
+var path = requre('path');
+var cors = require('cors');
 var ParseServer = require('parse-server').ParseServer;
 
-var api = new ParseServer({
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
+var app = express();
+app.use(cors());
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+  javascriptKey: process.env.JAVASCRIPT_KEY || '',
+  restAPIKey: process.env.REST_API_KEY || '',
+  dotNetKey: process.env.DOT_NET_KEY || '',
+  clientKey: process.env.CLIENT_KEY || ''
 });
 
-app.use('', api);
+app.use('/parse', api);
 
 app.get('/', function(req, res) {
     res.status(200).send('Server is running here');
