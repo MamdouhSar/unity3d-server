8cc0a34c96f3a092d2f28923bf19bf8441dcd29f
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-01-16 20:39:34 +0200

    updated server

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 var app = express();
 app.use(cors());
 
-var httpServer = require('http').createServer(app);
-var io = require('socket.io')(httpServer);
-
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://heroku_d7sm06pf:cak0b4unnjhnoouompb4gcm6tp@ds159348.mlab.com:59348/heroku_d7sm06pf',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
@@ -26,6 +23,21 @@ var api = new ParseServer({
   dotNetKey: process.env.DOT_NET_KEY || 'b6YhWaaebP',
   clientKey: process.env.CLIENT_KEY || '5pk333og1c'
 });
+
+app.use('/parse', api);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = process.env.PORT || 1335;
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('Unity3D Server is running');
+});
+
+var io = require('socket.io')(httpServer);
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -46,16 +58,3 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 });
-
-app.use('/parse', api);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-var port = process.env.PORT || 1335;
-
-httpServer.listen(port, function() {
-    console.log('Unity3D Server is running');
-});
-
