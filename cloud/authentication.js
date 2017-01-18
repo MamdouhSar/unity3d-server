536dbc8d0c260f1fa026813ab348c43c586f0746
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-01-18 16:10:26 +0200

    added signUp and logIn Cloud Functions

--- cloud/authentication.js
+++ cloud/authentication.js
@@ -2,6 +2,46 @@
  * Created by mamdouh on 15/01/17.
  */
 
+Parse.Cloud.define('signUp', function(request, response) {
+    var userObject = {
+        'username': request.params.username,
+        'email': request.params.email,
+        'password': request.params.password,
+    };
+    var newUser = new Parse.User();
+    newUser.set('username', userObject.username);
+    newUser.set('email', userObject.email);
+    newUser.set('password', userObject.password);
+    newUser.signUp().then(
+        function(user) {
+            response.success({
+                'result': 'SIGNED UP',
+                'user': user
+            });
+        },
+        function(error) {
+            response.error({
+                'result': 'ERROR',
+                'message' : error.message
+            });
+        }
+    );
+});
+
 Parse.Cloud.define('logIn', function(request, response) {
-    
+    var userObject = {
+        'username': request.params.username,
+        'password': request.params.password
+    };
+    Parse.User.logIn(userObject.username, userObject.password).then(
+        function(user) {
+            response.success({
+                'result': 'LOGGED IN',
+                'user': user
+            });
+        },
+        function(error) {
+            response.error(error);
+        }
+    );
 });
