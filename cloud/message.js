dfbab9949854eb4c108f6589e97a0f69509f6dbf
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-02-03 15:45:28 +0200

    added messageRead function

--- cloud/message.js
+++ cloud/message.js
@@ -1,6 +1,7 @@
 /**
  * Created by mamdouh on 15/01/17.
  */
+var async = require('async');
 
 Parse.Cloud.define('saveMessage', function(request, response) {
     var message = {
@@ -16,6 +17,7 @@ Parse.Cloud.define('saveMessage', function(request, response) {
     messageObject.set('sentBy', {'__type': 'Pointer', 'className':'_User', 'objectId': message.sentBy});
     messageObject.set('sentTo', {'__type': 'Pointer', 'className':'_User', 'objectId': message.sentTo});
     messageObject.set('content', message.content);
+    messageObject.set('read', false);
     messageObject.save().then(
         function(result) {
             response.success({
@@ -30,4 +32,49 @@ Parse.Cloud.define('saveMessage', function(request, response) {
             });
         }
     );
+});
+
+Parse.Cloud.define('messagesRead', function(request, response) {
+  var messageObject = new Parse.Object('Message');
+  var conversationId = request.params.conversationId;
+  var userId = request.params.userId;
+  messageObject.equalTo('conversationId', {'__type':'Pointer', 'className':'Conversation', 'objectId': conversationId});
+  messageObject.equalTo('read', false);
+  messageObject.equalTo('sentTo', {'__type':'Pointer', 'className':'_User', 'objectId': userId});
+  messageObject.find().then(
+    function(unreadMessages) {
+      async.each(unreadMessages, function(singleMessage, messageCallback) {
+        singleMessage.set('read',true);
+        singleMessage.save().then(
+          function() {
+            messageCallback();
+          },
+          function(error) {
+            response.success({
+              'message':'ERROR',
+              'result': error.message
+            });
+          }
+        );
+      },
+      function(error) {
+        if(error) {
+          response.success({
+            'message':'ERROR',
+            'result': error
+          });
+        } else {
+          response.success({
+            'message': 'SUCCESS'
+          })
+        }
+      });
+    },
+    function(error) {
+      response.success({
+        'message': 'ERROR',
+        'result': error.message
+      });
+    }
+  );
 });
