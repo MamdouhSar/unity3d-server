0467bdf6edbfa4a4ea76ac43872aeba97137a715
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-02-11 19:42:06 +0200

    push notifications

--- cloud/push.js
+++ cloud/push.js
@@ -1,3 +1,14 @@
 /**
  * Created by mamdouh on 05/02/17.
  */
+Parse.Cloud.define('testPush', function(request, response) {
+  Parse.Push.send({
+    where: {
+      "deviceType": { "$in": [ "ios",  "android"  ]  }
+    },
+    data: {
+      "title": "Ant-man",
+      "alert": "This is awesome. It is awesome."
+    }
+  }, { useMasterKey: true });
+});
