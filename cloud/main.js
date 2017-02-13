ceff63e866c860bf1ac260ee9cb2cadc9bf8b70d
MamdouhSar <mamdouh.alsarayreh@gmail.com>
2017-02-13 17:16:14 +0200

    push notification after save message

--- cloud/main.js
+++ cloud/main.js
@@ -10,22 +10,51 @@ require('./search.js');
 require('./admin/users.js');
 
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
-    var user = request.object;
-    var toLowerCase = function(w) { return w.toLowerCase(); };
-    if (Parse.FacebookUtils.isLinked(user)) {
-        Parse.Cloud.httpRequest({
-            url:'https://graph.facebook.com/me?fields=email,name&access_token='+user.get('authData').facebook.access_token,
-            success:function(httpResponse){
-                user.setUsername(httpResponse.data.name);
-                user.setEmail(httpResponse.data.email);
-                user.set('usernameLower', httpResponse.data.name.trim().toLowerCase());
-                response.success();
-            },
-            error:function(httpResponse){
-                console.error(httpResponse);
-            }
-        });
-    } else {
+  var user = request.object;
+  var toLowerCase = function(w) { return w.toLowerCase(); };
+  if (Parse.FacebookUtils.isLinked(user)) {
+    Parse.Cloud.httpRequest({
+      url:'https://graph.facebook.com/me?fields=email,name&access_token='+user.get('authData').facebook.access_token,
+      success:function(httpResponse){
+        user.setUsername(httpResponse.data.name);
+        user.setEmail(httpResponse.data.email);
+        user.set('usernameLower', httpResponse.data.name.trim().toLowerCase());
         response.success();
+      },
+      error:function(httpResponse){
+        console.error(httpResponse);
+      }
+    });
+  } else {
+    response.success();
+  }
+});
+
+Parse.Cloud.afterSave('Message', function(request, response) {
+  var user = request.object.get("sentBy");
+  //Set push query
+  user.fetch().then(
+    function(username) {
+      var pushQuery = new Parse.Query(Parse.Installation);
+      pushQuery.equalTo("username",username);
+      //Send Push message
+      Parse.Push.send({
+        where: pushQuery,
+        data: {
+          alert: "You got a message from " + username.get('username'),
+          sound: "default"
+        }
+      },{
+        success: function(){
+          response.success('true');
+        },
+        error: function (error) {
+          response.error(error);
+        }
+      });
+    },
+    function(error) {
+      response.error(error);
     }
+  );
 });
