af3e4b8f4ed043a83d57ccc1eb74364d6195aa8d
MamdouhSar <mamdoh.saraireh@yahoo.com>
2017-03-15 01:52:19 +0200

    validate password

--- server.js
+++ server.js
@@ -48,6 +48,25 @@ var api = new ParseServer({
     verbose: false
 });
 
+function validatePassword(password) {
+  var p = password,
+    errors = [];
+  if (p.length < 8) {
+    errors.push("Your password must be at least 8 characters");
+  }
+  if (p.search(/[a-z]/i) < 0) {
+    errors.push("Your password must contain at least one letter.");
+  }
+  if (p.search(/[0-9]/) < 0) {
+    errors.push("Your password must contain at least one digit.");
+  }
+  if (errors.length > 0) {
+    alert(errors.join("\n"));
+    return false;
+  }
+  return true;
+}
+
 app.use('/public', express.static('public'));
 app.use('/parse', api);
 
